2b69c5fbd5b82f6c57231db19493aa7c1b88bd42
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-12-10 17:26:04 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Servidor express rodando");
+});
+
+http
+  .createServer(app)
+  .listen(3000, () => console.log("Servidor rodando local na porta 3000"));
